81f3303c432e8954927993dd084969cbf465ae8c
EveryEunbin <116586151+EveryEunbin@users.noreply.github.com>
2025-11-13 01:14:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-#from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
-chromedriver_autoinstaller.install()
-#driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 chrome_options = Options()
 options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
+    "--headless"
 ]
 for option in options:
     chrome_options.add_argument(option)
@@ -26,6 +13,4 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('http://github.com')
 print(driver.title)
 
-driver.stop_client()
-driver.close()
 driver.quit()
