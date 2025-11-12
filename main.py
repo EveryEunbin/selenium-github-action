b9bc21db2a69117802a83ad99ea6e9a7527f8566
EveryEunbin <116586151+EveryEunbin@users.noreply.github.com>
2025-11-13 01:37:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
+from bs4 import BeautifulSoup
+import csv
+import json
+import time
+
+
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
@@ -27,7 +33,54 @@ service = Service('/usr/local/bin/chromedriver')
     
 driver = webdriver.Chrome(service=service, options=options)
 
-driver.get('http://github.com')
-print(driver.title)
+title_search = 'ดาราจักรรักลำนำใจ'
+url = f'https://wetv.vip/th/search/{title_search}'
+main_url = 'https://wetv.vip'
+titles = []
+links = []
+
+driver.get(url)
+
+collapses = driver.find_elements(By.CSS_SELECTOR, "li.search-result__video--collapse")
+if len(collapses)>0:
+    print('Having Collapse(s)')
+    for collapse in collapses:
+        driver.execute_script("arguments[0].click();", collapse)
+        time.sleep(3)
+
+html_doc = driver.page_source
+soup = BeautifulSoup(html_doc, 'html.parser')
+names_soup = soup.select('.search-result__title>span:first-child')
+main_titles = [name.get_text() for name in names_soup]
+
+uls = soup.select('ul.search-result__videos')
+
+if len(uls)==len(main_titles):
+    for i, ul in enumerate(uls):
+        print(main_titles[i])
+        all_a = ul.select('a.search-result__link')
+        for a_tag in all_a:
+            title = f'EP{a_tag['title']} {main_titles[i]}'
+            link = f'{main_url}{a_tag['href']}'
+            titles.append(title)
+            links.append(link)
+else:
+    print('Cannot process')
 
 driver.quit()
+
+with open('links.csv', 'w', newline='') as csvfile:
+    fieldnames = ['title', 'link']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for i in range(len(titles)):
+        writer.writerow({'title': titles[i], 'link': links[i]})
+
+with open('links.csv', mode='r', newline='') as csvfile:
+    data = list(csv.DictReader(csvfile))
+
+with open('links.json', mode='w') as jsonfile:
+    json.dump(data, jsonfile, indent=4, ensure_ascii=False)
+
+
+
